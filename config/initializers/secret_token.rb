3949515af574fff4af52564bebe00b5375ca305a
Alex Chow <alex.chow@gmail.com>
2011-04-21 20:41:00 -0700

    new ruby 3.0 vanilla application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTODO::Application.config.secret_token = 'b0ca46732e2844db8c80a586c20175d49f7ef50f901d35f81e743c7084d945d66661849916084f429013f55acd11d89254336cb0baa49c70b1999e5830b59df3'
